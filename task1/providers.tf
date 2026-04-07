743570e0aed5fa37dbc12b36d6ac6ccee90cf79a
NPlesh <plesh204@gmail.com>
2026-04-07 11:00:04 +0300

    task1: fix-up provider
    Reference: EXAM-01

--- task1/providers.tf
+++ task1/providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateplesh"
+    storage_account_name = "tfstatenplesh"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
